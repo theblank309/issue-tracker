edc80fbf3ee959fed59741e59fb4663e38ac89a2
Siddhesh Morye <63141152+theblank309@users.noreply.github.com>
2024-03-25 15:12:42 +0530

    Modified new issue page

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import Link from "next/link";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, TextField, TextArea } from "@radix-ui/themes";
+
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useMemo } from "react";
 
 const NewIssuePage = () => {
+  const MDEOptions = useMemo(() => {
+    return {
+      spellChecker: true,
+      status: false,
+    };
+  }, []);
+
   return (
     <div className="max-w-xl mt-10 space-y-3">
       <TextField.Root placeholder="Title" />
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" options={MDEOptions} />
       <Button>
         <Link href="/">Submit New Issue</Link>
       </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.variable}>
         <Theme>
           <NavBar />
           <main className="pl-5">{children}</main>

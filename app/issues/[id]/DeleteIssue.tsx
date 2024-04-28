36466cfb8ff136b2205d166aaab16a71d8eb91e8
Siddhesh Morye <63141152+theblank309@users.noreply.github.com>
2024-04-28 23:57:53 +0530

    Added modal for delete issue

--- app/issues/[id]/DeleteIssue.tsx
+++ app/issues/[id]/DeleteIssue.tsx
@@ -1,12 +1,34 @@
 import { PrimaryButton } from "@/app/components/Buttons";
 import { TrashIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import React from "react";
 
 const DeleteIssue = () => {
   return (
-    <PrimaryButton>
-      <TrashIcon /> Delete Issue
-    </PrimaryButton>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <TrashIcon /> Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex mt="4" gapX="2">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete Issue</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
